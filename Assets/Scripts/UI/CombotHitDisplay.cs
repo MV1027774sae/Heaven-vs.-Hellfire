20784038d5d24f9ae98c58e441f49cc7f9dc1161
An Vo <107334075+truonganvo@users.noreply.github.com>
2023-10-18 21:24:07 +1100

    finding way to implement combo hit system

--- Assets/Scripts/UI/CombotHitDisplay.cs
+++ Assets/Scripts/UI/CombotHitDisplay.cs
@@ -1,15 +1,12 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows;
 
 public class CombotHitDisplay : MonoBehaviour
 {
     [SerializeField] int numberOfHit = 0;
+    [SerializeField] float coolDown = 1.5f;
 
     [SerializeField] StateManager stateManagerScript;
-    [SerializeField] float coolDown = 2f;
 
     private void Update()
     {
@@ -23,14 +20,14 @@ public class CombotHitDisplay : MonoBehaviour
         if (stateManagerScript.gettingHit == true)
         {
             numberOfHit += 1;
-            coolDown = 2f;
+            coolDown = 1.5f;
             StartCoroutine(ResetBoolean());
         }
 
         else if (coolDown <= 0)
         {
             numberOfHit = 0;
-            coolDown = 2f;
+            coolDown = 1.5f;
         }
     }
 
