b50f510c34e6c0b937bfe8f30332bc2d5790600f
An Vo <107334075+truonganvo@users.noreply.github.com>
2023-10-25 17:31:16 +1100

    working on controller support

---
+++ Assets/ControllerInput/PlayerController.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEditor.VersionControl.Asset;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] StateManager states;
+    [SerializeField] HandleMovement handleMovement;
+    [SerializeField] HandleAnimations anim;
+
+    public float speed;
+    private float horizontal;
+
+    public float jumpForce = 5.0f; // Adjust this value as needed.
+    private bool isJumping = false;
+
+
+    void Awake()
+    {
+
+    }
+
+    //Help moving
+    private void FixedUpdate()
+    {
+        speed = 143f;
+
+        if (states.onGround && !states.currentlyAttacking && !states.blocking)
+        {
+            rb.AddForce(new Vector2((horizontal * speed) - rb.velocity.x * handleMovement.acceleration, 0));
+
+            if (states.onGround && states.lookRight && horizontal < 0)
+            {
+                states.guard = true;
+            }
+            else if (states.onGround && !states.lookRight && horizontal > 0)
+            {
+                states.guard = true;
+            }
+            else if (!states.onGround || horizontal == 0)
+            {
+                states.guard = false;
+            }
+        }
+
+        //in case there's sliding
+        if (horizontal == 0 && states.onGround)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+
+    //Call Jumping
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Jump();
+        }
+    }
+
+    //Call Moving
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("Moving");
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    //Help Jumping and Check if the character hit ground
+    private void Jump()
+    {
+        if(!isJumping)
+        {
+            Debug.Log("Jumping");
+            anim.JumpAnim();
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
