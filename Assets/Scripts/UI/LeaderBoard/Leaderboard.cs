2ed74c18f5835c9b66208ce447e85430d7bf79e7
An Vo <107334075+truonganvo@users.noreply.github.com>
2023-11-22 18:50:31 +1100

    working on leaderboard

---
+++ Assets/Scripts/UI/LeaderBoard/Leaderboard.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] List<TextMeshProUGUI> names;
+    [SerializeField] List<TextMeshProUGUI> scores;
+
+    [SerializeField] string publicLeaderboardKey = "d285b08aee81372c24e2271d34eaf7ff9f0bc8082081fe21190dc46726e2f25d";
+
+    private void Start()
+    {
+        GetLeaderboard();
+    }
+
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
+            for (int i = 0; i < loopLength; ++i)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            GetLeaderboard();
+        }));
+    }
+}
