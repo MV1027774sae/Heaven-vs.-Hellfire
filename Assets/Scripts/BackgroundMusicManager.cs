d035470d721eff939fe2f511504f1f296804c3db
Max Valentine <107592271+MV1027774sae@users.noreply.github.com>
2024-04-28 22:02:55 +1000

    Various Bug Fixes

--- Assets/Scripts/BackgroundMusicManager.cs
+++ Assets/Scripts/BackgroundMusicManager.cs
@@ -16,6 +16,7 @@ public class BackgroundMusicManager : MonoBehaviour
 
     private void PlayRandomMusic()
     {
-        source.PlayOneShot(musicList[Random.Range(0, musicList.Length)]);
+        source.clip = (musicList[Random.Range(0, musicList.Length)]);
+        source.Play();
     }
 }
